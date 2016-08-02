fe7c879c846763bcd0f3c4a8bb13ff5724d6868c
Egill Runar Vidarsson <kertavax@gmail.com>
2016-08-02 12:16:00 -0400

    fullscreen video capture, aspect ratio wonky

--- js/p5-device-video-capture.js
+++ js/p5-device-video-capture.js
@@ -1,14 +1,19 @@
-var capture;
+var video;
+var canvas;
+var canvas_w = screen.width;
+var canvas_h = canvas_w / 1.6;
 
 function setup() {
-	createCanvas(390, 240);
+	canvas = createCanvas(canvas_w, canvas_h);
+	canvas.class("fluid");
+	
 	capture = createCapture(VIDEO);
-	capture.size(320, 240);
+	capture.size(canvas_w, canvas_h);
 	//capture.hide();
 }
 
 function draw() {
 	background(255);
-	image(capture, 0, 0, 320, 240);
+	image(capture, 0, 0, canvas_w, canvas_h);
 	// filter('INVERT');
 }
